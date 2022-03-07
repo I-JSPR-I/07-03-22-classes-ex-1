afab4dbb2666d7859c18275631f944e481afb2c1
I-JSPR-I <jaspermeersschaut@hotmail.com>
2022-03-07 10:44:33 +0100

    Add project files.

---
+++ 07-03-22 classes ex 1/Bank.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07_03_22_classes_ex_1
+{
+    class Bank
+    {
+
+    }
+}
---
+++ 07-03-22 classes ex 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _07_03_22_classes_ex_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
