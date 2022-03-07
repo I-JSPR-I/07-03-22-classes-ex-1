7493b616ead0b335777493a8cf65f970726a1040
I-JSPR-I <jaspermeersschaut@hotmail.com>
2022-03-07 11:17:32 +0100

    11:15 07/03/22

--- 07-03-22 classes ex 1/Bank.cs
+++ 07-03-22 classes ex 1/Bank.cs
@@ -6,6 +6,19 @@ namespace _07_03_22_classes_ex_1
 {
     class Bank
     {
+        public int Stand;
+        public int Limit;
+        public int Verichtingen;
 
+        public Bank()
+        {
+            Stand = 0;
+            Limit = 1000;
+            Verichtingen = 0;
+        }
+        public Bank(int GestortGeld)
+        {
+            Stand = Stand + GestortGeld;
+        }
     }
-}
+    }
--- 07-03-22 classes ex 1/Program.cs
+++ 07-03-22 classes ex 1/Program.cs
@@ -6,7 +6,55 @@ namespace _07_03_22_classes_ex_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Bank bankrekening = new Bank();
+            do
+            {
+                int Antwoord = Menu();
+                if (Antwoord == 1)
+                {
+                    GeldStorten(Antwoord);
+                }
+                if (Antwoord == 2)
+                {
+                    GeldAfhalen(Antwoord);
+                }
+                if (Antwoord == 3)
+                {
+                    StatusBekijken(Antwoord);
+                }
+
+            } while (Convert.ToInt32(Console.ReadLine()) != 0);
+        }
+
+        private static void StatusBekijken(int Antwoord)
+        {
+        }
+
+        private static void GeldAfhalen(int Antwoord)
+        {
+
+        }
+
+        private static int GeldStorten(int Antwoord)
+        {
+            Console.Clear();
+            Console.WriteLine("Hoeveel geld wilt u storten?");
+            int gestortgeld = Convert.ToInt32(Console.ReadLine());
+
+            return gestortgeld;
+        }
+
+        private static int Menu()
+        {
+            Console.WriteLine("Wat wilt u doen?");
+            Console.WriteLine("(1) Geld storten");
+            Console.WriteLine("(2) Geld Afhalen");
+            Console.WriteLine("(3) Rekening bekijken");
+            Console.WriteLine("(0) Menu sluiten");
+
+
+            int Antwoord = Convert.ToInt32(Console.ReadLine());
+            return Antwoord;
         }
     }
 }
