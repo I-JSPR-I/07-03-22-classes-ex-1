0bf2cdafac321408fb08a82105992d6e77dbb9ea
I-JSPR-I <jaspermeersschaut@hotmail.com>
2022-04-15 13:53:53 +0200

    finished

--- 07-03-22 classes ex 1/Bank.cs
+++ 07-03-22 classes ex 1/Bank.cs
@@ -6,19 +6,34 @@ namespace _07_03_22_classes_ex_1
 {
     class Bank
     {
-        public int Stand;
-        public int Limit;
-        public int Verichtingen;
+        private int stand;
+        private int limit;
+        private int verichtingen;
 
+        public int Stand   // property
+        {
+            get { return stand; }   // get method
+            set { stand = value; }  // set method
+        }
+        public int Limit   // property
+        {
+            get { return limit; }   // get method
+            set { limit = value; }  // set method
+        }
+        public int Verichtingen   // property
+        {
+            get { return verichtingen; }   // get method
+            set { verichtingen = value; }  // set method
+        }
         public Bank()
         {
-            Stand = 0;
-            Limit = -1000;
-            Verichtingen = 0;
+            stand = 0;
+            limit = -1000;
+            verichtingen = 0;
         }
         public Bank(int GestortGeld)
         {
-            Stand = Stand + GestortGeld;
+            stand = stand + GestortGeld;
         }
     }
 }
